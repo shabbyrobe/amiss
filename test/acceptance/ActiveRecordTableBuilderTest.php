7e9348334f722eab2adca89a93d434e85bdaba63
Blake Williams <blake@shabbyrobe.org>
2011-12-19 03:33:05 +1100

    Removed old license

--- test/acceptance/ActiveRecordTableBuilderTest.php
+++ test/acceptance/ActiveRecordTableBuilderTest.php
@@ -1,25 +1,5 @@
 <?php
 
-/*
- * This file is part of Amiss.
- * 
- * Amiss is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Amiss is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with Amiss.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Copyright 2011 Blake Williams
- * http://k3jw.com 
- */
-
 namespace Amiss\Test\Acceptance;
 
 use Amiss\Active\TableBuilder,
