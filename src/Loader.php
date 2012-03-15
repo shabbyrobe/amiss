614a4969b1459e6e36c4cf7395b6b3af55d5b7ca
Blake Williams <blake@shabbyrobe.org>
2012-03-15 22:09:35 +1100

    removed diagnostic

--- src/Loader.php
+++ src/Loader.php
@@ -7,10 +7,7 @@ class Loader
 	public function load($class)
 	{
 		if (strpos($class, 'Amiss\\')===0) {
-			$file = __DIR__.'/'.str_replace('\\', '/', str_replace('..', '', substr($class, 6))).'.php';
-			if (!file_exists($file))
-				throw new \Exception($file);
-			require($file);
+			require(__DIR__.'/'.str_replace('\\', '/', str_replace('..', '', substr($class, 6))).'.php');
 			return true;
 		}
 	}
