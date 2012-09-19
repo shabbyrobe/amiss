b67fda59f46c84391160639079edd81f0c9d165d
blake <blake@blake-ubu.(none)>
2012-09-19 10:54:31 +1000

    hmm

--- src/Type/Encoder.php
+++ src/Type/Encoder.php
@@ -3,43 +3,43 @@ namespace Amiss\Type;
 
 class Encoder implements Handler
 {
-	public $serialiser;
-	public $deserialiser;
-	public $innerHandler;
+    public $serialiser;
+    public $deserialiser;
+    public $innerHandler;
 
-	public function __construct($serialiser, $deserialiser, $innerHandler=null)
-	{
-		$this->serialiser = $serialiser;
-		$this->deserialiser = $deserialiser;
-		$this->innerHandler = $innerHandler;
-	}
+    public function __construct($serialiser, $deserialiser, $innerHandler=null)
+    {
+        $this->serialiser = $serialiser;
+        $this->deserialiser = $deserialiser;
+        $this->innerHandler = $innerHandler;
+    }
 
-	function prepareValueForDb($value, $object, array $fieldInfo)
-	{
-		$return = null;
-		if ($value) {
-			if ($this->innerHandler)
-				$value = $this->innerHandler->prepareValueForDb($value, $object, $fieldInfo);
+    function prepareValueForDb($value, $object, array $fieldInfo)
+    {
+        $return = null;
+        if ($value) {
+            if ($this->innerHandler)
+                $value = $this->innerHandler->prepareValueForDb($value, $object, $fieldInfo);
 
-			$return = call_user_func($this->serialiser, $value);
-		}
-		return $return;
-	}
-	
-	function handleValueFromDb($value, $object, array $fieldInfo, $row)
-	{
-		$return = null;
-		if ($value) {
-			$return = call_user_func($this->deserialiser, $value);
+            $return = call_user_func($this->serialiser, $value);
+        }
+        return $return;
+    }
+    
+    function handleValueFromDb($value, $object, array $fieldInfo, $row)
+    {
+        $return = null;
+        if ($value) {
+            $return = call_user_func($this->deserialiser, $value);
 
-			if ($this->innerHandler)
-				$return = $this->innerHandler->handleValueFromDb($return, $object, $fieldInfo, $row);
-		}
-		return $return;
-	}
-	
-	function createColumnType($engine)
-	{
-		return 'TEXT';
-	}
+            if ($this->innerHandler)
+                $return = $this->innerHandler->handleValueFromDb($return, $object, $fieldInfo, $row);
+        }
+        return $return;
+    }
+    
+    function createColumnType($engine)
+    {
+        return 'TEXT';
+    }
 }
