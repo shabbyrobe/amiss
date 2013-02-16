eb2bdc5e926885b9cff1134b2670b85372f00070
shabbyrobe <code@shabbyrobe.org>
2013-02-16 11:22:12 +1100

    Fixes #7

--- test/unit/ArrayMapperTest.php
+++ test/unit/ArrayMapperTest.php
@@ -176,6 +176,77 @@ class ArrayMapperTest extends \CustomTestCase
      * @group unit
      * @covers Amiss\Mapper\Arrays::createMeta
      */
+    public function testArrayPrimaryExplicitFieldType()
+    {
+        $mappings = array(
+            'foo'=>array('primary'=>'id', 'fields'=>array('id'=>array('type'=>'foobar'))),
+        );
+        $mapper = new Arrays($mappings);
+        $mapper->defaultPrimaryType = 'flobadoo';
+        $meta = $mapper->getMeta('foo');
+        
+        $expected = array(
+            'id'=>array('name'=>'id', 'type'=>'foobar'),
+        );
+        $this->assertEquals($expected, $meta->getFields());
+    }
+    
+    /**
+     * Tests issue #7
+     * @group unit
+     * @covers Amiss\Mapper\Arrays::createMeta
+     */
+    public function testArrayPrimaryExplicitFieldTypeWithFieldName()
+    {
+        $mappings = array(
+            'foo'=>array(
+                'primary'=>'id', 
+                'fields'=>array(
+                    'id'=>array('name'=>'pants', 'type'=>'foobar')
+                )
+            ),
+        );
+        $mapper = new Arrays($mappings);
+        $mapper->defaultPrimaryType = 'flobadoo';
+        $meta = $mapper->getMeta('foo');
+        
+        $expected = array(
+            'id'=>array('name'=>'pants', 'type'=>'foobar'),
+        );
+        
+        $this->assertEquals($expected, $meta->getFields());
+    }
+    
+    /**
+     * Tests issue #7
+     * @group unit
+     * @covers Amiss\Mapper\Arrays::createMeta
+     */
+    public function testArrayPrimaryImplicitFieldTypeWithFieldName()
+    {
+        $mappings = array(
+            'foo'=>array(
+                'primary'=>'id', 
+                'fields'=>array(
+                    'id'=>array('name'=>'pants')
+                )
+            ),
+        );
+        $mapper = new Arrays($mappings);
+        $mapper->defaultPrimaryType = 'flobadoo';
+        $meta = $mapper->getMeta('foo');
+        
+        $expected = array(
+            'id'=>array('name'=>'pants', 'type'=>'flobadoo'),
+        );
+        
+        $this->assertEquals($expected, $meta->getFields());
+    }
+    
+    /**
+     * @group unit
+     * @covers Amiss\Mapper\Arrays::createMeta
+     */
     public function test()
     {
         
