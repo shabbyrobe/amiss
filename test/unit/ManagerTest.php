1da376b6326ead3a351f3be9d0a2689505f98873
shabbyrobe <code@k3jw.com>
2013-05-20 23:12:45 +1000

    added phpunit coverage whitelist added extra files to gitignore added tests for exceptions

--- test/unit/ManagerTest.php
+++ test/unit/ManagerTest.php
@@ -71,4 +71,38 @@ class ManagerTest extends \CustomTestCase
         );
         $this->assertEquals($expected, $result);
     }
+
+    /**
+     * @group unit
+     * @group manager
+     * @covers Amiss\Sql\Manager::getRelated
+     */
+    public function testGetRelatedFailsWhenRelationNameUnknown()
+    {
+        $source = new \stdClass;
+        $this->setExpectedException("Amiss\Exception", "Unknown relation flobadoo on stdClass");
+        $this->manager->getRelated($source, 'flobadoo');
+    }
+
+    /**
+     * @group unit
+     * @group manager
+     * @covers Amiss\Sql\Manager::getRelated
+     */
+    public function testGetRelatedFailsWhenRelatorUnknown()
+    {
+        $this->manager->mapper = $this->getMockBuilder("Amiss\Mapper")
+            ->setMethods(array('getMeta'))
+            ->getMockForAbstractClass()
+        ;
+        $meta = new \Amiss\Meta('stdClass', 'stdClass', array());
+        $meta->relations = array(
+            'a'=>array('wahey')
+        );
+        $this->manager->mapper->expects($this->any())->method('getMeta')->will($this->returnValue($meta));
+
+        $source = new \stdClass;
+        $this->setExpectedException("Amiss\Exception", "Relator wahey not found");
+        $this->manager->getRelated($source, 'a');
+    }
 }
